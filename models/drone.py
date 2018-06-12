648cdf9c7c2c540ac9cd48890da2dfb33461721c
emmettburns <39931703+emmettburns@users.noreply.github.com>
2018-06-12 14:00:00 -0400

    drone new methods
    drone.py now contains methods that allow calculation of coverage and uniformity

--- models/drone.py
+++ models/drone.py
@@ -40,3 +40,63 @@ class Drone(BaseStation):
     def do_step(self,obstacle):
         if not self.dead:
             self.algorithm_provider.run(self,obstacle)
+
+    #returns a list of all the pixels in the com range
+    def getCoverage(self, rng):
+        comRange = set({})
+        droneX = int(self.x)
+        droneY = int(self.y)
+        startingX = int(self.x-(rng/2))
+        startingY = int(self.y-(rng/2))
+        currentX = startingX
+        currentY = startingY
+        for i in range(rng):
+            for j in range(rng):
+                if math.hypot(droneX-currentX, droneY-currentY) < rng/2:
+                    comRange.add((currentX, currentY))
+                currentY += 1
+            currentX += 1
+            currentY = startingY
+        return comRange
+
+    #returns the neighbors of the drone
+    def getNeighbors(self, drones, rng):
+        # returns a list of drones within communications range
+        dronecoord = self.get_coords()
+        drones_in_range = []
+        for t in [i for i in drones if not i.dead and type(i) is Drone]:
+            tcoord = t.get_coords()
+            euclidian = math.hypot(dronecoord[0]-tcoord[0], dronecoord[1]-tcoord[1])
+            
+            if euclidian < rng and self is not t:
+                drones_in_range.append(t)
+
+        return drones_in_range
+
+    #returns the mean internodal distance
+    def meanInternodalDistance(self, neighbors, rng):
+        if neighbors:
+            distSum = 0
+            for drone in neighbors:
+                tcoord = drone.get_coords()
+                euclidian = math.hypot(self.x-tcoord[0], self.y-tcoord[1])
+                distSum += euclidian
+            return distSum/len(neighbors)
+        else:
+            return None
+
+    #calculates individual drones contribution to uniformity 
+    def droneUniformity(self, drones, rng):
+        neighbors = self.getNeighbors(drones, rng)
+        total = 0
+        numberOfNeighbors = len(neighbors)
+        m = self.meanInternodalDistance(neighbors, rng)
+        if neighbors:
+            for drone in neighbors:
+                tcoord = drone.get_coords()
+                d = math.hypot(self.x-tcoord[0], self.y-tcoord[1])
+                total += (d - m)**2
+            output = (total/numberOfNeighbors)**0.5
+            return output
+        else:
+            return 0

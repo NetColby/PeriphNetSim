5ded417580c71a28c73263872292af89efc89a80
emmettburns <39931703+emmettburns@users.noreply.github.com>
2018-06-28 13:10:12 -0400

    takes in the name of the setting to be changed
    and the  new setting as command line arguments and updates settings.txt

---
+++ settingsEditor.py
@@ -0,0 +1,32 @@
+#Emmett Burns
+#06/27/2018
+#Settings Updator
+
+import sys
+
+class settingsEditor:
+	
+	def __init__(self):
+		pass
+		
+	def editSetting(self, setting, newSetting):
+		file = open("settings.txt", "r")
+		text = file.read()
+		settingWOequals = setting
+		setting += "="
+		halves = text.split(setting)
+		portionToBeChanged = halves[1].split("\n")
+		oldSetting = portionToBeChanged[0]
+		portionToBeChanged[0] = newSetting
+		newSecondHalf = "\n".join(portionToBeChanged)
+		newSettingsList = [halves[0], newSecondHalf]
+		newSettings = setting.join(newSettingsList)
+		file = open("settings.txt", "w")
+		file.write(newSettings)
+		file.close()
+		print("\n\n____________________SETTINGS UPDATE____________________\n\n" + 
+			settingWOequals + " changed from " + oldSetting + " to " + newSetting + "\n\n")
+		
+if __name__ ==  "__main__":
+	se = settingsEditor()
+	se.editSetting(sys.argv[1], sys.argv[2])
